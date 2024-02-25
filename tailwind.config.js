8db6fa5d3990845dff6b35a209b437675c94643a
reaget <reaget.acc@gmail.com>
2024-02-25 16:17:25 +0300

    feat: finish config. add layout elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat"],
+      },
+      fontSize: {
+        base: "10px",
+      },
+      colors: {
+        yellow: {
+          DEFAULT: "#ECCF4D",
+        },
+        gray: {
+          DEFAULT: "#EFECEC",
+        }
+      }
+    },
   },
   plugins: [],
 }
