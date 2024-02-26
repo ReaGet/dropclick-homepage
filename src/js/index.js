2d3e72cd9bd7daee711aff798bd25bca24208b06
Rifat Mukhametzyanov <rifat@avtoalfa.com>
2024-02-26 16:25:18 +0300

    makefeat: add toggle mobile menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,34 @@
 import "../css/index.css";
+
+const actions = {
+  toggleMenu,
+};
+
+document.addEventListener("click", (event) => {
+  const action = getClosestAction(event.target);
+  if (!action) {
+    return;
+  }
+
+  event.currentEl = action.target;
+
+  actions[action.name](event);
+});
+
+function getClosestAction(el) {
+  const actionName = el?.getAttribute("data-action");
+  if (actionName) {
+    return { name: actionName, target: el };
+  }
+
+  if (el.parentElement) {
+    return getClosestAction(el.parentNode);
+  }
+
+  return null;
+}
+
+function toggleMenu(event) {
+  const wrapper = event.currentEl.closest(".group");
+  wrapper.classList.toggle("js-active");
+}
