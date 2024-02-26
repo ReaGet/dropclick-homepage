fa38886715e8c4c290322a258412b3ec2229ce80
Rifat Mukhametzyanov <rifat@avtoalfa.com>
2024-02-26 16:54:46 +0300

    feat: start creating slider

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import "../css/index.css";
 
 const actions = {
   toggleMenu,
+  changeSlide,
 };
 
 document.addEventListener("click", (event) => {
@@ -31,4 +32,41 @@ function getClosestAction(el) {
 function toggleMenu(event) {
   const wrapper = event.currentEl.closest(".group");
   wrapper.classList.toggle("js-active");
+}
+
+let activeIndex = 0;
+
+const slider = {
+  next: nextSlide,
+  prev: prevSlide,
+};
+
+function changeSlide(event) {
+  const articles = document.querySelectorAll(".guides-wrapper article");
+  const { value } = event.currentEl.dataset;
+
+  slider[value](articles, );
+  console.log(outerWidth)
+}
+
+function nextSlide(articles) {
+  const outerWidth = getSlidesWidth(articles);
+  activeIndex = Math.min(++activeIndex, articles.length);
+  console.log("next", activeIndex);
+}
+
+function prevSlide() {
+  activeIndex = Math.max(--activeIndex, 0);
+  console.log("prev", activeIndex);
+}
+
+function updateSliderButtons() {
+  
+}
+
+function getSlidesWidth(articles) {
+  return [...articles].reduce((sum, item) => {
+    sum += item.offsetWidth;
+    return sum;
+  }, 0);
 }
