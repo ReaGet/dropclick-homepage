4952e69ebc8409667973f7d9c9cd929a7ef3d465
Rifat Mukhametzyanov <rifat@avtoalfa.com>
2024-02-26 16:27:09 +0300

    build: first build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import tailwindcss from "tailwindcss";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
+    build: {
+        outDir: "docs",
+    },
     plugins: [
         tailwindcss(),
         svgr(),
