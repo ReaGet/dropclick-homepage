70e83ed1685a93e33fd6fb8e87f1bc158e8c8c26
Rifat Mukhametzyanov <rifat@avtoalfa.com>
2024-02-27 08:40:19 +0300

    feat: add results page. build project and change import paths

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,34 @@
 import { defineConfig } from "vite";
 import tailwindcss from "tailwindcss";
 import svgr from "vite-plugin-svgr";
+import { resolve } from "path";
 
 export default defineConfig({
     build: {
         outDir: "docs",
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                results: resolve(__dirname, "results/index.html"),
+            },
+            output: {
+                assetFileNames: (assetInfo) => {
+                    let extType = assetInfo.name.split('.').at(1);
+                    console.log(assetInfo.name)
+                    if (/png|jpe?g|gif|tiff|bmp|ico/i.test(extType)) {
+                        extType = "img";
+                    } else if (/svg/i.test(extType)) {
+                        extType = "svg";
+                    } else if (/woff2|ttf|woff/i.test(extType)) {
+                        extType = "fonts";
+                    } else if (/css/i.test(extType)) {
+                        extType = "css";
+                    }
+                    return `assets/${extType}/[name]-[hash][extname]`;
+                },
+                entryFileNames: "assets/js/[name]-[hash].js",
+            },
+        },
     },
     plugins: [
         tailwindcss(),
